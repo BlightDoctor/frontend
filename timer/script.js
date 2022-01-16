3eb9d80e20dda105b21d739831be915fd826d41b
BlightDoctor <mrhanki23@mail.ru>
2022-01-16 15:32:37 +0300

    luxury

---
+++ timer/script.js
@@ -0,0 +1,48 @@
+function countdown(dateEnd) {
+  var timer, days, hours, minutes, seconds;
+
+  dateEnd = new Date(dateEnd);
+  dateEnd = dateEnd.getTime();
+
+  if (isNaN(dateEnd)) {
+    return;
+  }
+
+  timer = setInterval(calculate, 1000);
+
+  function calculate() {
+    var dateStart = new Date();
+    var dateStart = new Date(dateStart.getUTCFullYear(),
+      dateStart.getMonth(),
+      dateStart.getDate(),
+      dateStart.getHours(),
+      dateStart.getMinutes(),
+      dateStart.getSeconds());
+    var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
+
+    if (timeRemaining >= 0) {
+      days = parseInt(timeRemaining / 86400);
+      timeRemaining = (timeRemaining % 86400);
+      hours = parseInt(timeRemaining / 3600);
+      timeRemaining = (timeRemaining % 3600);
+      minutes = parseInt(timeRemaining / 60);
+      timeRemaining = (timeRemaining % 60);
+      seconds = parseInt(timeRemaining);
+
+
+      document.getElementById("days").innerHTML = parseInt(days, 10);
+      document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+      document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+      document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+    } else {
+      return;
+    }
+  }
+
+  function display(days, hours, minutes, seconds) {}
+}
+
+
+
+
+countdown ('01/01/2023 00:00:00 AM');
